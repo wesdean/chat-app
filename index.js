f6ab91dc1e6a22bc3a81374ba39df834b7882ec4
Wes Dean <wes@geekdaddydean.com>
2020-03-03 10:26:17 -0600

    created react app

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const port = process.env.PORT || 4000;
+
+// App setup
+const app = express();
+const server = app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`);
+});
+
+// Static files
+app.use(express.static('public'));
+
+// Socket setup
+const io = socket(server);
+
+io.on('connection', socket => {
+    console.log(`Made socket connection: ${socket.id}`);
+
+    socket.on('chat', data => {
+        console.log(`Message received: ${data.handle} -> ${data.message}`);
+        io.sockets.emit('chat', data);
+    });
+
+    socket.on('typing', data => {
+        socket.broadcast.emit('typing', data);
+    })
+});
