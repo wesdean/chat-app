03d3f5a904d504ffc402599ceef4cd59fa28da51
Wes Dean <wes@geekdaddydean.com>
2020-03-04 08:15:47 -0600

    changed websocket url

---
+++ public/precache-manifest.044953f512ba51676cb87ccbe61c7819.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e151aae3e767586b2c032cdf51062e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71580dd60500fee0ff4c",
+    "url": "/static/js/2.9f44cc46.chunk.js"
+  },
+  {
+    "revision": "4e251f0da6f1a35ce7e065b90cb9ba3d",
+    "url": "/static/js/2.9f44cc46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aac0e5c3a5851ca26550",
+    "url": "/static/js/main.46a100f1.chunk.js"
+  },
+  {
+    "revision": "b681ecfa501425697523",
+    "url": "/static/js/runtime-main.33200e42.js"
+  }
+]);
