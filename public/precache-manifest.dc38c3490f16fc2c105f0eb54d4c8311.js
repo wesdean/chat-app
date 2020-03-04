32fee42a396e9f5206fb6168b873c0ebb34c7ba7
Wes Dean <wes@geekdaddydean.com>
2020-03-04 07:07:46 -0600

    added chat windows

---
+++ public/precache-manifest.dc38c3490f16fc2c105f0eb54d4c8311.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98df407c6207c2a29286ea85cfed2805",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71580dd60500fee0ff4c",
+    "url": "/static/js/2.9f44cc46.chunk.js"
+  },
+  {
+    "revision": "4e251f0da6f1a35ce7e065b90cb9ba3d",
+    "url": "/static/js/2.9f44cc46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "352af4f1c856d37609fa",
+    "url": "/static/js/main.fe2abdf7.chunk.js"
+  },
+  {
+    "revision": "b681ecfa501425697523",
+    "url": "/static/js/runtime-main.33200e42.js"
+  }
+]);
